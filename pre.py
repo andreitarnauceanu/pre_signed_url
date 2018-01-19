de2b1dc2560b77418d35ac052b96cb5d8146edb5
Cloud User <ec2-user@ip-10-0-1-77.eu-central-1.compute.internal>
2018-01-19 13:43:46 +0000

    Write the filename and shorturl in a list

--- pre.py
+++ pre.py
@@ -1,31 +1,23 @@
 import boto3
-
 import json
 import requests
+from utils import shorten, pre_signed_url
 
-GOOGLE_URL_SHORTEN_API = 'AIzaSyBrnkM64WKqmKa_FmRbR6WIZeAu-hXE-6I' # IP Address restrictions
-
-def shorten(url):
-   req_url = 'https://www.googleapis.com/urlshortener/v1/url?key=' + GOOGLE_URL_SHORTEN_API
-   payload = {'longUrl': url}
-   headers = {'content-type': 'application/json'}
-   r = requests.post(req_url, data=json.dumps(payload), headers=headers)
-   resp = json.loads(r.text)
-   return resp['id']
-
-def pre_signed_url(key, bucket):
-	s3Client = boto3.client('s3')
-	return s3Client.generate_presigned_url('get_object', Params = {'Bucket': bucket, 'Key': key}, ExpiresIn = 300)
-
-
+original_files = []
+croped_files = []
 s3 = boto3.resource('s3')
 bucket_name = 'microservice-backet-linuxacademyuser'
 my_bucket = s3.Bucket(bucket_name)
-
 for file in my_bucket.objects.all():
   if 'CROP' in file.key:
-    print " %s | %s" % (file.key, shorten(pre_signed_url(file.key, bucket_name)))
+    shorturl = shorten(pre_signed_url(file.key, bucket_name))
+    original_files.append(dict(key=file.key, url=shorturl))
 print '-'*30
 for file in my_bucket.objects.all():
   if 'uploads' in file.key:
-    print " %s | %s" % (file.key, shorten(pre_signed_url(file.key, bucket_name)))
+    shorturl = shorten(pre_signed_url(file.key, bucket_name))
+    croped_files.append(dict(key=file.key, url=shorturl))
+	
+print original_files
+print croped_files
+
